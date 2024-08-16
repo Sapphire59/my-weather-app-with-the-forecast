15ebb730bb050c751f2399d81d12bcfa71378ae6
Deborah <liz.joy43@gmail.com>
2024-08-16 17:49:50 -0600

    Removed data from HTML and updated the data in JS

--- src/app.js
+++ src/app.js
@@ -63,6 +63,29 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tues"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">☀️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">84°</div>
+      <div class="weather-forecast-temperature">60°</div>
+    </div>
+  </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Colorado");
+
+displayForecast();
